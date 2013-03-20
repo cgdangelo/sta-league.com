b28c44915c0dcbdaedaa66d551c214b633202b9e
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-03-20 10:23:29 -0400

    Add stock rails with gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sta::Application.config.secret_token = '143ecd1a86cd01a42bee1bde2ba42c1415e723e188a78cea8d96398a54edbb223092970c9c9f0c96e9efdb67f7db865b4281d950975750c1a6ade8a6e280c3ee'
