acf41388c8fa9fa019e303e35f0186e74202181b
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-03-21 20:07:50 -0400

    Add role check method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
                   :alias, :first_name, :last_name
 
   has_and_belongs_to_many :roles
+
+  def role?(role)
+    return !!self.roles.find_by_name(role.to_s.camelize)
+  end
 end
